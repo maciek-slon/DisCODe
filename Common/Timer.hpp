0bb950e1e5b2bf3178aeed2b2282f0b93bd644fb
mstefanc <maciek.slon@gmail.com>
2010-05-16 04:11:25 +0200

    Timer extended to provide better accuracy (system dependent code), UNIFIED_TIMER switch added to CMake variables (causes Timer to use only ctime library with portable clock() function)

--- Common/Timer.hpp
+++ Common/Timer.hpp
@@ -9,7 +9,14 @@
 #ifndef TIMER_HPP_
 #define TIMER_HPP_
 
-#include <ctime>
+#if defined(UNIFIED_TIMER)
+#	include <ctime>
+#elif defined(WIN32)
+#	define WIN32_LEAN_AND_MEAN
+#	include <windows.h>
+#else
+#	include <ctime>
+#endif
 
 namespace Common {
 
@@ -18,6 +25,7 @@ namespace Common {
  */
 class Timer {
 public:
+#if defined(UNIFIED_TIMER)
 	/*!
 	 * Default constructor.
 	 *
@@ -25,6 +33,7 @@ public:
 	 */
 	Timer() {
 		start_time = std::clock();
+		freq = CLOCKS_PER_SEC;
 	}
 
 	/*!
@@ -39,13 +48,100 @@ public:
 	/*!
 	 * Return elapsed time in seconds
 	 */
-	double elapsed() const {
-		return double(std::clock() - start_time) / CLOCKS_PER_SEC;
+	double elapsed() {
+		return double(std::clock() - start_time) / freq;
 	}
 
+	long frequency() const {
+		return freq;
+	}
+#elif defined(WIN32)
+	/*!
+	 * Default constructor.a
+	 *
+	 * \post elapsed() == 0
+	 */
+	Timer() {
+		QueryPerformanceFrequency(&freq);
+	}
+
+	/*!
+	 * Restart timer
+	 *
+	 * \post elapsed() == 0
+	 */
+	void restart() {
+		QueryPerformanceCounter(&start_time);
+	}
+
+	/*!
+	 * Return elapsed time in seconds
+	 */
+	double elapsed() {
+		QueryPerformanceCounter(&end_time);
+		return double(end_time.QuadPart - start_time.QuadPart) / freq.QuadPart;
+	}
+
+	long frequency() const {
+		return freq.QuadPart;
+	}
+#else
+	/*!
+	 * Default constructor.
+	 *
+	 * \post elapsed() == 0
+	 */
+	Timer() {
+		clock_getres(CLOCK_REALTIME, &start_time);
+		freq = (long)(1.0 / (start_time.tv_sec + 0.001*0.001*0.001*start_time.tv_nsec));
+	}
+
+	/*!
+	 * Restart timer
+	 *
+	 * \post elapsed() == 0
+	 */
+	void restart() {
+		clock_gettime(CLOCK_REALTIME, &start_time);
+	}
+
+	/*!
+	 * Return elapsed time in seconds
+	 */
+	double elapsed() {
+		clock_gettime(CLOCK_REALTIME, &end_time);
+		double ns = 0.001*0.001*0.001*(end_time.tv_nsec - start_time.tv_nsec);
+		return double(end_time.tv_sec - start_time.tv_sec + ns);
+	}
+
+	long frequency() const {
+		return freq;
+	}
+#endif /* WIN32 */
+
 private:
-	/// Start time
-	std::clock_t start_time;
+#if defined (UNIFIED_TIMER)
+	/// ticks per second
+	long freq;
+	/// start time
+	clock_t start_time;
+	/// end time
+	clock_t end_time;
+#elif defined(WIN32)
+	/// ticks per second
+    LARGE_INTEGER freq;
+    /// start time
+    LARGE_INTEGER start_time;
+    /// end time
+    LARGE_INTEGER end_time;
+#else /* *nix */
+    /// ticks per second
+    long freq;
+    /// start time
+	timespec start_time;
+	/// end time
+	timespec end_time;
+#endif /* WIN32 */
 };
 
 } //: namespace Common
