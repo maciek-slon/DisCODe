201bce34fc657d62cf1c597e297e175302ad7257
maciek-slon <maciek.slon@gmail.com>
2010-06-01 13:27:50 +0200

    Props.hpp added to repository

---
+++ Base/Props.hpp
@@ -0,0 +1,87 @@
+/*!
+ * \file Props.hpp
+ * \brief
+ * \author mstefanc
+ * \date 2010-05-28
+ */
+
+#ifndef PROPS_HPP_
+#define PROPS_HPP_
+
+#include <boost/property_tree/ptree.hpp>
+
+namespace Base {
+
+using boost::property_tree::ptree;
+
+/*!
+ * \class Props
+ * \brief Base class for property structures.
+ *
+ * All structs that needs to be loaded and saved to config files should inherit
+ * from Props and implement two methods - load and save. Those methods take ptree
+ * as argument, so it's rather flexible - config can be kept in separate files for
+ * every module or it can be subnode of bigger property tree.
+ *
+ * \par Defining own property structures
+ *
+ * Assuming that we need two properties - name of object and it's serial number
+ * we can create following structure:
+ *
+ * \code
+ * struct Properties {
+ * 	// module name
+ * 	std::string name;
+ *
+ * 	// module serial number
+ *	int serial;
+ * };
+ * \endcode
+ *
+ * To allow this parameters to be read and written from config file we have to inherit
+ * Base::Props and implement save and load methods.
+ *
+ * \code
+ * struct Properties : public Base::Props {
+ * 	// module name
+ * 	std::string name;
+ *
+ * 	// module serial number
+ * 	int serial;
+ *
+ * 	// load settings
+ * 	void load(const ptree & pt) {
+ * 		name = pt.get("props.name", "Default name");
+ * 		serial = pt.get("props.serial", 0);
+ * 	}
+ *
+ * 	// save settings
+ * 	void save(ptree & pt) {
+ * 		pt.put("props.name", name);
+ * 		pt.put("props.serial", serial);
+ * 	}
+ * };
+ * \endcode
+ *
+ */
+class Props {
+
+public:
+	/*!
+	 * Load settings
+	 *
+	 * @param pt root property tree to load settings from
+	 */
+	virtual void load(const ptree & pt) = 0;
+
+	/*!
+	 * Save settings
+	 *
+	 * @param pt root property tree to save settings
+	 */
+	virtual void save(ptree & pt) = 0;
+};
+
+}//: namespace Base
+
+#endif /* PROPS_HPP_ */
