cc1a4f7c4fbd3e9480572225e5d0d8a0718ac216
maciek-slon <maciek.slon@gmail.com>
2010-07-06 18:31:49 +0200

    Policies for DataStream

---
+++ Base/Policies/Synchronization.hpp
@@ -0,0 +1,82 @@
+/*!
+ * \file Synchronization
+ * \brief Synchronization policies
+ *
+ * \author mstefanc
+ * \date 06-07-2010
+ */
+
+#ifndef SYNCHRONIZATION_HPP_
+#define SYNCHRONIZATION_HPP_
+
+#include <boost/thread/mutex.hpp>
+
+namespace Base {
+
+/*!
+ * Synchronization policies
+ *
+ * Every buffering policy should have form:
+ * \code
+ * template <class T>
+ * class PolicyName {
+ * protected:
+ *   // store data in some internal container
+ *   void store(const T & t);
+ *
+ *   // retrieve data from internal container
+ *   T retrieve();
+ * };
+ * \endcode
+ */
+namespace Synchronization {
+
+/*!
+ * \brief Synchronization policy - use mutex.
+ *
+ * Every access is synchronized using internal, per-object mutex.
+ */
+class Mutex {
+public:
+	/*!
+	 * Lock mutex, wait if it's busy
+	 */
+	void lock() {
+		mtx.lock();
+	}
+
+	/*!
+	 * Unlock mutex
+	 */
+	void unlock() {
+		mtx.unlock();
+	}
+
+private:
+	//// Mutex for access synchronization
+	boost::mutex mtx;
+};
+
+/*!
+ * \brief Synchronization policy - no synchronization.
+ *
+ * Empty methods, no synchronization is performed at all.
+ */
+class NoSync {
+public:
+	/*!
+	 * Do nothing
+	 */
+	void lock() {}
+
+	/*!
+	 * Do nothing
+	 */
+	void unlock() {}
+};
+
+
+} //: Synchronization
+} //: Base
+
+#endif /* SYNCHRONIZATION_HPP_ */
