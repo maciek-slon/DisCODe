64e2ff88daf8c0045c149f7979d057ce0fbc0438
maciek-slon <maciek.slon@gmail.com>
2010-08-20 16:24:15 +0200

    First task-specific processor created and used.

--- Types/stream_OpenCV.hpp
+++ Types/stream_OpenCV.hpp
@@ -9,20 +9,63 @@
  * Operators for standard streams
  */
 
-template <typename Ch, typename Traits, typename t>
-std::basic_ostream<Ch, Traits> & operator<<(std::basic_ostream<Ch, Traits> & out, const cv::Size_<t> & s) {
-	out << s.width << " " << s.height;
-	return out;
+/*!
+ * Stream operator printing cv::Size_<T>
+ * @param s output stream
+ * @param e size to be printed
+ * @return output stream
+ */
+template <typename Ch, typename Traits, typename T>
+std::basic_ostream<Ch, Traits> & operator<<(std::basic_ostream<Ch, Traits> & s, const cv::Size_<T> & e) {
+	s << e.width << " " << e.height;
+	return s;
 }
 
-template <typename Ch, typename Traits, typename t>
-std::basic_istream<Ch, Traits> & operator>>(std::basic_istream<Ch, Traits> & in, cv::Size_<t> & s) {
-	t w,h;
-	in >> w >> h;
-	s.width = w;
-	s.height = h;
-	return in;
+/*!
+ * Stream operator reading cv::Size_<T>
+ * @param s input stream
+ * @param e size to be read
+ * @return input stream
+ */
+template <typename Ch, typename Traits, typename T>
+std::basic_istream<Ch, Traits> & operator>>(std::basic_istream<Ch, Traits> & s, cv::Size_<T> & e) {
+	T w,h;
+	s >> w >> h;
+	e.width = w;
+	e.height = h;
+	return s;
 }
+
+/*!
+ * Stream operator printing cv::Rect_<T>
+ * @param s output stream
+ * @param e rect to be printed
+ * @return output stream
+ */
+template <typename Ch, typename Traits, typename T>
+std::basic_ostream<Ch, Traits> & operator<<(std::basic_ostream<Ch, Traits> & s, const cv::Rect_<T> & e) {
+	s << e.x << " " << e.y << " " << e.width << " " << e.height;
+	return s;
+}
+
+/*!
+ * Stream operator reading cv::Rect_<T>
+ * @param s input stream
+ * @param e rect to be read
+ * @return input stream
+ */
+template <typename Ch, typename Traits, typename T>
+std::basic_istream<Ch, Traits> & operator>>(std::basic_istream<Ch, Traits> & s, cv::Rect_<T> & e) {
+	T x,y,w,h;
+	s >> x >> y >> w >> h;
+
+	e.x = x;
+	e.y = y;
+	e.width = w;
+	e.height = h;
+	return s;
+}
+
 
 /*
  * Operators for Boost::PropertyTree
@@ -31,17 +74,37 @@ std::basic_istream<Ch, Traits> & operator>>(std::basic_istream<Ch, Traits> & in,
 namespace boost {
 namespace property_tree {
 
-template <typename Ch, typename Traits, typename t>
-struct customize_stream<Ch, Traits, cv::Size_<t>, void>
+template <typename Ch, typename Traits, typename T>
+struct customize_stream<Ch, Traits, cv::Size_<T>, void>
 {
 
-	static void insert(std::basic_ostream<Ch, Traits>& s, const cv::Size_<t>& e) {
+	static void insert(std::basic_ostream<Ch, Traits>& s, const cv::Size_<T>& e) {
 		s << e.width << " " << e.height;
 	}
 
-	static void extract(std::basic_istream<Ch, Traits>& s, cv::Size_<t>& e) {
-		t w,h;
+	static void extract(std::basic_istream<Ch, Traits>& s, cv::Size_<T>& e) {
+		T w,h;
 		s >> w >> h;
+		e.width = w;
+		e.height = h;
+	}
+};
+
+
+template <typename Ch, typename Traits, typename T>
+struct customize_stream<Ch, Traits, cv::Rect_<T>, void>
+{
+
+	static void insert(std::basic_ostream<Ch, Traits>& s, const cv::Rect_<T>& e) {
+		s << e.x << " " << e.y << " " << e.width << " " << e.height;
+	}
+
+	static void extract(std::basic_istream<Ch, Traits>& s, cv::Rect_<T>& e) {
+		T x,y,w,h;
+		s >> x >> y >> w >> h;
+
+		e.x = x;
+		e.y = y;
 		e.width = w;
 		e.height = h;
 	}
