6dd826835755404a77ce4cbabbedee9149aaa031
maciek-slon <maciek.slon@gmail.com>
2010-12-16 02:00:58 +0100

    CTest + Boost.Test unit testing.
    - CommandInterpreter as first tested feature
    - directory 'tests' with CMakeLists ready to add more tests

--- tests/CommandInterpreter_test.cpp
+++ tests/CommandInterpreter_test.cpp
@@ -1,23 +1,28 @@
+#define BOOST_TEST_MODULE CommandInterpreter_test
+#include <boost/test/included/unit_test.hpp>
+
 #include "../src/Core/CommandInterpreter.hpp"
 
 #include <vector>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 std::string print(std::vector<std::string> args) {
+	std::stringstream ss;
 	for (int i = 0; i < args.size(); ++i) {
-		std::cout << args[i] << std::endl;
+		ss << args[i] << ",";
 	}
 
-	return "OK";
+	std::cout << ss.str();
+
+	return ss.str();
 }
 
-int main() {
+BOOST_AUTO_TEST_CASE( test ) {
 	Core::CommandInterpreter interpreter;
 
 	interpreter.addHandler("print", print);
-	interpreter.execute("print:ela:to:zdzira");
-	interpreter.execute("print3:ela:to:zdzira");
-
-	return 0;
+	BOOST_CHECK(interpreter.execute("print:ala:ma:kota") == "ala,ma,kota,");
+	BOOST_CHECK_THROW(interpreter.execute("print3:ala:ma:kota"), const char *);
 }
