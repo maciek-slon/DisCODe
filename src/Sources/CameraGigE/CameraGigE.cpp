8f62212b3dfb740a69ec5eebdd0780aae7f5d549
Konrad Banachowicz <konradb3@gmail.com>
2010-12-26 11:18:38 +0100

    Some properties added.

--- src/Sources/CameraGigE/CameraGigE.cpp
+++ src/Sources/CameraGigE/CameraGigE.cpp
@@ -10,11 +10,11 @@
 namespace Sources {
 namespace CameraGigE {
 
-CameraGigE::CameraGigE(const std::string & name) : Base::Component(name) {
+CameraGigE::CameraGigE(const std::string & name) :
+	Base::Component(name) {
 	LOG(LTRACE) << "Hello CameraGigE from dl\n";
 
-	if(PvInitialize() == ePvErrResources)
-	{
+	if (PvInitialize() == ePvErrResources) {
 		LOG(LERROR) << "Unable to initialize GigE !!! \n";
 		//return false;
 	}
@@ -37,34 +37,190 @@ bool CameraGigE::onInit() {
 
 	registerStream("out_img", &out_img);
 
-	if(!props.address.empty())
-	{
+	if (!props.address.empty()) {
 		unsigned long ip = inet_addr(props.address.c_str());
 
-		if(PvCameraOpenByAddr(ip,ePvAccessMaster,&cHandle) != ePvErrSuccess)
-		{
-			LOG(LERROR) << "Unable to open camera on adress " << props.address << " \n";
+		if (PvCameraOpenByAddr(ip, ePvAccessMaster, &cHandle) != ePvErrSuccess) {
+			LOG(LERROR) << "Unable to open camera on adress "
+					<< props.address << " \n";
 			return false;
 		}
 
-	} else if(props.uid != 0)
-	{
-		if(PvCameraOpen(props.uid, ePvAccessMaster, &cHandle) != ePvErrSuccess)
-		{
-			LOG(LERROR) << "Unable to open camera with uid " << props.uid << " \n";
+	} else if (props.uid != 0) {
+		if (PvCameraOpen(props.uid, ePvAccessMaster, &cHandle) != ePvErrSuccess) {
+			LOG(LERROR) << "Unable to open camera with uid " << props.uid
+					<< " \n";
 			return false;
 		}
-	} else
-	{
+	} else {
 		return false;
 	}
 
-	PvAttrEnumSet(cHandle,"FrameStartTriggerMode","Freerun");
+	// Set parameters
+	tPvErr err;
+	///	 Exposure
+	if (!props.exposureMode.empty()) {
+		if ((err = PvAttrEnumSet(cHandle, "ExposureMode",
+				props.exposureMode.c_str())) == ePvErrSuccess) {
+			if (props.exposureMode == "Manual") {
+				if ((err = PvAttrUint32Set(cHandle, "ExposureValue",
+						props.exposureValue / 1000000.0)) != ePvErrSuccess) {
+					if (err == ePvErrOutOfRange) {
+						tPvUint32 min, max;
+						PvAttrRangeUint32(cHandle, "ExposureValue", &min, &max);
+						LOG(LWARNING) << "ExposureValue : "
+								<< props.exposureValue
+								<< " is out of range, valid range [ "
+								<< (double) min / 1000000.0 << " , "
+								<< (double) max / 1000000.0 << " ]\n";
+					}
+				}
+			}
+		} else {
+			LOG(LWARNING) << "sfdfsdfsdsd";
+		}
+	}
+	/// Gain
+	if (!props.gainMode.empty()) {
+		if ((err = PvAttrEnumSet(cHandle, "GainMode", props.gainMode.c_str()))
+				== ePvErrSuccess) {
+			if (props.gainMode == "Manual") {
+				if ((err = PvAttrUint32Set(cHandle, "gainValue",
+						props.gainValue)) != ePvErrSuccess) {
+					if (err == ePvErrOutOfRange) {
+						tPvUint32 min, max;
+						PvAttrRangeUint32(cHandle, "GainValue", &min, &max);
+						LOG(LWARNING) << "GainValue : " << props.gainValue
+								<< " is out of range, valid range [ "
+								<< (double) min << " , " << (double) max
+								<< " ]\n";
+					}
+				}
+			}
+		} else {
+			LOG(LWARNING) << "sfdfsdfsdsd";
+		}
+	}
+	///	White Balance
+	if (!props.whitebalMode.empty()) {
+		if ((err = PvAttrEnumSet(cHandle, "WhitebalMode",
+				props.gainMode.c_str())) == ePvErrSuccess) {
+			if (props.whitebalMode == "Manual") {
+				if ((err = PvAttrUint32Set(cHandle, "WhitebalValueRed",
+						props.whitebalValueRed)) != ePvErrSuccess) {
+					if (err == ePvErrOutOfRange) {
+						tPvUint32 min, max;
+						PvAttrRangeUint32(cHandle, "WhitebalValueRed", &min,
+								&max);
+						LOG(LWARNING) << "WhitebalValueRed : "
+								<< props.whitebalValueRed
+								<< " is out of range, valid range [ "
+								<< (double) min << " , " << (double) max
+								<< " ]\n";
+					}
+				}
+
+				if ((err = PvAttrUint32Set(cHandle, "WhitebalValueBlue",
+						props.whitebalValueBlue)) != ePvErrSuccess) {
+					if (err == ePvErrOutOfRange) {
+						tPvUint32 min, max;
+						PvAttrRangeUint32(cHandle, "WhitebalValueBlue", &min,
+								&max);
+						LOG(LWARNING) << "WhitebalValueBlue : "
+								<< props.whitebalValueBlue
+								<< " is out of range, valid range [ "
+								<< (double) min << " , " << (double) max
+								<< " ]\n";
+					}
+				}
+			}
+		} else {
+			LOG(LWARNING) << "sfdfsdfsdsd";
+		}
+	}
+
+	if ((err = PvAttrEnumSet(cHandle, "MirrorX", props.mirrorX ? "On" : "Off"))
+			!= ePvErrSuccess) {
+
+	}
+
+	if ((err = PvAttrEnumSet(cHandle, "PixelFormat", props.pixelFormat.c_str()))
+			!= ePvErrSuccess) {
+
+	}
+
+	if ((err = PvAttrUint32Set(cHandle, "Height", props.height))
+			!= ePvErrSuccess) {
+		if (err == ePvErrOutOfRange) {
+			tPvUint32 min, max;
+			PvAttrRangeUint32(cHandle, "Height", &min, &max);
+			LOG(LWARNING) << "Height : " << props.height
+					<< " is out of range, valid range [ " << (double) min
+					<< " , " << (double) max << " ]\n";
+		}
+	}
+
+	if ((err = PvAttrUint32Set(cHandle, "Width", props.width)) != ePvErrSuccess) {
+		if (err == ePvErrOutOfRange) {
+			tPvUint32 min, max;
+			PvAttrRangeUint32(cHandle, "Width", &min, &max);
+			LOG(LWARNING) << "Width : " << props.width
+					<< " is out of range, valid range [ " << (double) min
+					<< " , " << (double) max << " ]\n";
+		}
+	}
+
+	if ((err = PvAttrUint32Set(cHandle, "RegionX", props.regionX))
+			!= ePvErrSuccess) {
+		if (err == ePvErrOutOfRange) {
+			tPvUint32 min, max;
+			PvAttrRangeUint32(cHandle, "RegionX", &min, &max);
+			LOG(LWARNING) << "RegionX : " << props.regionX
+					<< " is out of range, valid range [ " << (double) min
+					<< " , " << (double) max << " ]\n";
+		}
+	}
+
+	if ((err = PvAttrUint32Set(cHandle, "RegionY", props.regionY))
+			!= ePvErrSuccess) {
+		if (err == ePvErrOutOfRange) {
+			tPvUint32 min, max;
+			PvAttrRangeUint32(cHandle, "RegionY", &min, &max);
+			LOG(LWARNING) << "RegionY : " << props.regionY
+					<< " is out of range, valid range [ " << (double) min
+					<< " , " << (double) max << " ]\n";
+		}
+	}
+
+	if ((err = PvAttrUint32Set(cHandle, "BinningX", props.binningX))
+			!= ePvErrSuccess) {
+		if (err == ePvErrOutOfRange) {
+			tPvUint32 min, max;
+			PvAttrRangeUint32(cHandle, "BinningX", &min, &max);
+			LOG(LWARNING) << "BinningX : " << props.binningX
+					<< " is out of range, valid range [ " << (double) min
+					<< " , " << (double) max << " ]\n";
+		}
+	}
+
+	if ((err = PvAttrUint32Set(cHandle, "BinningY", props.binningY))
+			!= ePvErrSuccess) {
+		if (err == ePvErrOutOfRange) {
+			tPvUint32 min, max;
+			PvAttrRangeUint32(cHandle, "BinningY", &min, &max);
+			LOG(LWARNING) << "BinningY : " << props.binningY
+					<< " is out of range, valid range [ " << (double) min
+					<< " , " << (double) max << " ]\n";
+		}
+	}
+	// ----------------
+
+	PvAttrEnumSet(cHandle, "FrameStartTriggerMode", "Freerun");
 
 	unsigned long frameSize = 0;
 
-	if(PvAttrUint32Get(cHandle,"TotalBytesPerFrame",&frameSize) != ePvErrSuccess)
-	{
+	if (PvAttrUint32Get(cHandle, "TotalBytesPerFrame", &frameSize)
+			!= ePvErrSuccess) {
 		return false;
 	}
 
@@ -83,47 +239,40 @@ bool CameraGigE::onFinish() {
 bool CameraGigE::onStep() {
 	LOG(LTRACE) << "CameraGigE::onStep";
 
-	tPvErr Err = PvCaptureQueueFrame(cHandle,&frame,NULL);
-    if(!Err)
-    {
-        Err = PvCaptureWaitForFrameDone(cHandle,&frame,PVINFINITE);
-        if(!Err)
-        {
-            if(frame.Status == ePvErrSuccess)
-            {
-            	img = cv::Mat(frame.Height,
-						  frame.Width,
-						  CV_8UC3, frame.ImageBuffer);
+	tPvErr Err = PvCaptureQueueFrame(cHandle, &frame, NULL);
+	if (!Err) {
+		Err = PvCaptureWaitForFrameDone(cHandle, &frame, PVINFINITE);
+		if (!Err) {
+			if (frame.Status == ePvErrSuccess) {
+				img = cv::Mat(frame.Height, frame.Width, (frame.Format
+						== ePvFmtMono8) ? CV_8UC1 : CV_8UC3, frame.ImageBuffer);
 
-            	out_img.write(img);
-            	newImage->raise();
-            }
-        }
-    }
+				out_img.write(img);
+				newImage->raise();
+			}
+		}
+	}
 
 	return true;
 }
 
 bool CameraGigE::onStart() {
-    // set the camera is acquisition mode
-    if(!PvCaptureStart(cHandle))
-    {
-        // start the acquisition and make sure the trigger mode is "freerun"
-        if(PvCommandRun(cHandle,"AcquisitionStart"))
-        {
-            // if that fail, we reset the camera to non capture mode
-            PvCaptureEnd(cHandle) ;
-            return false;
-        }
-        else
-            return true;
-    } else
-    	return false;
+	// set the camera is acquisition mode
+	if (!PvCaptureStart(cHandle)) {
+		// start the acquisition and make sure the trigger mode is "freerun"
+		if (PvCommandRun(cHandle, "AcquisitionStart")) {
+			// if that fail, we reset the camera to non capture mode
+			PvCaptureEnd(cHandle);
+			return false;
+		} else
+			return true;
+	} else
+		return false;
 }
 
 bool CameraGigE::onStop() {
-    PvCommandRun(cHandle,"AcquisitionStop");
-    PvCaptureEnd(cHandle);
+	PvCommandRun(cHandle, "AcquisitionStop");
+	PvCaptureEnd(cHandle);
 	return true;
 }
 
--- src/Sources/CameraGigE/CameraGigE.hpp
+++ src/Sources/CameraGigE/CameraGigE.hpp
@@ -94,32 +94,86 @@ namespace Sources {
 namespace CameraGigE {
 
 struct Props : public Base::Props {
-
+	// Device
 	std::string address;
 	uint32_t uid;
 
-//	bool sort;
-//	bool prefetch;
+	// Controls
+	/// Exposure
+	std::string 	exposureMode;
+	double 			exposureValue;
+	/// Gain
+	std::string		gainMode;
+	int				gainValue;
+	/// Iris
 
-//	bool triggered;
+	/// WhiteBalance
+	std::string		whitebalMode;
+	int 			whitebalValueRed;
+	int				whitebalValueBlue;
 
+	// ImageFormat
+	bool			mirrorX;
+	std::string		pixelFormat;
+	///ROI
+	int 			height;
+	int 			width;
+	int				regionX;
+	int				regionY;
+
+	// ImageMode
+	int				binningX;
+	int				binningY;
 
 	void load(const ptree & pt) {
-		address = pt.get("address", "");
-		uid = pt.get("uid", 0);
+		address = pt.get("Address", "");
+		uid = pt.get("UID", 0);
 
-	//	sort = pt.get("sort", true);
-	//	prefetch = pt.get("prefetch", false);
+		exposureMode = pt.get("Controls.Exposure.Mode", "auto");
+		exposureValue = pt.get("Controls.Exposure.Value", 0.05);
 
-	//	triggered = pt.get("triggered", false);
+		gainMode = pt.get("Controls.Gain.Mode", "auto");
+		gainValue = pt.get("Controls.Gain.Value", 0);
+
+		whitebalMode = pt.get("Controls.WhiteBalance.Mode", "auto");
+		whitebalValueRed = pt.get("Controls.WhiteBalance.ValueRed", 50);
+		whitebalValueBlue = pt.get("Controls.WhiteBalance.ValueBlue", 50);
+
+		mirrorX = pt.get("ImageFormat.MirrorX", false);
+		pixelFormat = pt.get("ImageFormat.PixelFormat", "RGB24");
+		height = pt.get("ImageFormat.ROI.Height", 480);
+		width = pt.get("ImageFormat.ROI.Width", 640);
+		regionX = pt.get("ImageFormat.ROI.RegionX", 0);
+		regionY = pt.get("ImageFormat.ROI.RegionY", 0);
+
+		binningX = pt.get("ImageMode.BinningX", 1);
+		binningY = pt.get("ImageMode.BinningY", 1);
+
 	}
 
 	void save(ptree & pt) {
-		pt.put("address", address);
-		pt.put("uid", uid);
-		//pt.put("sort", sort);
-		//pt.put("prefetch", prefetch);
-		//pt.put("triggered", triggered);
+		pt.put("Address", address);
+		pt.put("UID", uid);
+
+		pt.put("Controls.Exposure.Mode", exposureMode);
+		pt.put("Controls.Exposure.Value", exposureValue);
+
+		pt.put("Controls.Gain.Mode", gainMode);
+		pt.put("Controls.Gain.Value", gainValue);
+
+		pt.put("Controls.WhiteBalance.Mode", whitebalMode);
+		pt.put("Controls.WhiteBalance.ValueRed", whitebalValueRed);
+		pt.put("Controls.WhiteBalance.ValueBlue", whitebalValueBlue);
+
+		pt.put("ImageFormat.MirrorX", mirrorX);
+		pt.put("ImageFormat.PixelFormat", pixelFormat);
+		pt.put("ImageFormat.ROI.Height", height);
+		pt.put("ImageFormat.ROI.Width", width);
+		pt.put("ImageFormat.ROI.RegionX", regionX);
+		pt.put("ImageFormat.ROI.RegionY", regionY);
+
+		pt.put("ImageMode.BinningX", binningX);
+		pt.put("ImageMode.BinningY", binningY);
 	}
 };
 
