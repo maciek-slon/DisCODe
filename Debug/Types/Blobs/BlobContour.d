9ec93b36c8ea6bfee5ea8aac7aa2865c897d15ad
maciek-slon <maciek.slon@gmail.com>
2011-03-18 17:55:46 +0100

    Removed useles dirs

--- Debug/Types/Blobs/BlobContour.d
+++
@@ -1,4 +0,0 @@
-Types/Blobs/BlobContour.d Types/Blobs/BlobContour.o: \
- ../Types/Blobs/BlobContour.cpp ../Types/Blobs/BlobContour.hpp
-
-../Types/Blobs/BlobContour.hpp:
--- Debug/doc_src/examples/logger_example_1.d
+++
@@ -1,2 +0,0 @@
-doc_src/examples/logger_example_1.d doc_src/examples/logger_example_1.o: \
- ../doc_src/examples/logger_example_1.cpp
